9239d0bf9e7eb8f3c47f5960615001b5b2a8f4c9
Filla Ramadhani <fillau123@gmail.com>
2024-03-13 21:44:37 +0700

    Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,22 +12,42 @@ class UserController extends Controller
 {
     public function index()
     {
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstwhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_dua',
         //     'nama' => 'Manager 2',
         //     'password' => Hash::make('12345'),
         // ];
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
 
-        $user = UserModel::all(); 
-        return view('user', ['data' => $user]);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all(); 
+        // return view('user', ['data' => $user]);
 
         // tambah data user dengan Eloquent Model 
         // $data = [
